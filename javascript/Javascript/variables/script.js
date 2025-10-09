21c7fdf0392b158545187c09f95865c27d5da160
ranjithkadam <21x31a6622@gmail.com>
2025-10-09 20:20:48 +0530

    variables and its types

---
+++ javascript/Javascript/variables/script.js
@@ -0,0 +1,54 @@
+
+// variables = the variables are to store the data and controll the data.it is a conatiner which has numbers and objects strings numbers etc
+
+// it has three types they are: 1)var  2)let  3)const
+
+// 1)var= vartype  is the blocked scope  and global scope it has both fuctions 
+// it has the ability  to re-assign(assignment) and the re-declare(declaration)
+
+
+
+// 2)let=lettype is the blocked scope 
+// it has the ability  to re-assign(assignment) but not the re-declare(declaration)
+
+// 3)const=consttype is the bocked scope 
+// it  do not have  the ability  to re-assign(assignment)  and  re-declare(declaration)
+
+// eg:
+
+// var tony = "100"
+
+// here the tony is the declaration and the 100 is the assignment
+
+
+
+var tony = "nightmare"
+
+console.log(tony)
+
+    var tony ="bright";
+    console.log(tony)  
+
+    var toni = "nightmare"
+    console.log(toni)
+
+    var v = 100;
+    console.log(v)
+
+
+   {let tony = "bright"
+    console.log(tony)
+
+let tonu = "bright"
+console.log(tonu)
+
+
+}  
+
+
+{
+
+const man = "the spark of the thunder can make  both people happy and sad "
+console.log(man)    
+
+}
