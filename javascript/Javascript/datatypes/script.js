e2b6657088ab79428d97156d9b1c74bc603473a7
ranjithkadam <21x31a6622@gmail.com>
2025-10-28 11:01:09 +0530

    face book task

---
+++ javascript/Javascript/datatypes/script.js
@@ -0,0 +1,43 @@
+// datatypes = values of variables can hold such as number text object
+
+// they are classified in to two types: 1)premitive data type 2)non primitive data type
+
+// 1)primitive data type :they are classified into five types
+
+// 1)number:represents the numaric values and integers floting points
+
+// 2)String:represents a sequence of characters such as text (enclosed in doublecodes or single code) 
+
+// 3)Boolean:: represents a logical value either true or false
+
+// 4)null::represents the absence of value or non existence Object
+
+// 5)undefined:: represents a variable that has been declare but not assigned avalue
+
+// 6)big int :: it allows very large numbers in integer
+
+
+
+let vadder= 2;//number
+console.log(vadder);
+
+
+let tony="man";//string
+console.log(tony);
+
+let man=true;//boolean
+console.log(man);
+
+var been=null;///null
+console.log(been);
+
+var spider= ///undefinied
+console.log(spider);
+
+let can=732654664648n;//bigint
+console.log(can);
+
+
+
+
+    
