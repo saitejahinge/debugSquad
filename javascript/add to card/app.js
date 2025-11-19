a17956caf33bfde482e23b25d2c6f92d1060d61b
ranjithkadam <21x31a6622@gmail.com>
2025-11-19 22:01:12 +0530

    add to cart with localstorages

---
+++ javascript/add to card/app.js
@@ -0,0 +1,28 @@
+
+
+function register(){
+let username = document.getElementById("name").value
+let password = document.getElementById("password").value
+
+localStorage.setItem("username", username);
+localStorage.setItem("password",password);
+alert("registerd succesfully") }
+
+function login(){
+
+let username = document.getElementById("name").value
+let password = document.getElementById("password").value
+let result=localStorage.getItem("username")
+let result2=localStorage.getItem("password")
+
+if(username== result && password == result2){
+    alert("login sucessful");
+    // window.location.href ="./index.html"
+    document.getElementById("form").submit();
+    
+}
+else{
+    alert("Invalid Credentials")
+}
+}
+document.getElementById("text").innerHTML=`<p>Welcome ${localStorage.getItem("username")}</p>`;
